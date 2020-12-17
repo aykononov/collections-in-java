060b61a53e09d0987915082f66312d188a7091cf
aykononov <aykononov@yandex.ru>
2020-12-17 08:10:40 +0300

    17.12.2020

--- src/main/java/CollectionSpeedTest.java
+++ src/main/java/CollectionSpeedTest.java
@@ -15,26 +15,36 @@ public class CollectionSpeedTest {
 
         long arrListTime = System.currentTimeMillis();
         arrList.add(5000, 1111);
-        System.out.println("arrList на вставку: " + (System.currentTimeMillis() - arrListTime));
+        System.out.println("arrList вставка: " + (System.currentTimeMillis() - arrListTime));
 
         arrListTime = System.currentTimeMillis();
-        arrList.get(500000);
-        System.out.println("arrList на поиск: " + (System.currentTimeMillis() - arrListTime));
+        arrList.get(900000);
+        System.out.println("arrList поиск: " + (System.currentTimeMillis() - arrListTime));
+
+        arrListTime = System.currentTimeMillis();
+        arrList.remove(5000);
+        System.out.println("arrList удаление: " + (System.currentTimeMillis() - arrListTime));
 
         long linkedListTime = System.currentTimeMillis();
         linkedList.add(5000, 1111);
-        System.out.println("\nlinkedList на вставку: " + (System.currentTimeMillis() - linkedListTime));
+        System.out.println("\nlinkedList вставка: " + (System.currentTimeMillis() - linkedListTime));
 
         linkedListTime = System.currentTimeMillis();
-        linkedList.get(500000);
-        System.out.println("linkedList на поиск: " + (System.currentTimeMillis() - linkedListTime));
+        linkedList.get(900000);
+        System.out.println("linkedList поиск: " + (System.currentTimeMillis() - linkedListTime));
+
+        linkedListTime = System.currentTimeMillis();
+        linkedList.remove(5000);
+        System.out.println("linkedList удаление: " + (System.currentTimeMillis() - linkedListTime));
     }
 }
 
 /*-----------------------
-arrList на вставку: 2
-arrList на поиск: 0
+arrList вставка: 1
+arrList поиск: 0
+arrList удаление: 4
 
-linkedList на вставку: 0
-linkedList на поиск: 9
+linkedList вставка: 0
+linkedList поиск: 6
+linkedList удаление: 0
  */
